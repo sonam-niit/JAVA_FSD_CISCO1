e9a3afad0c798da7c8c137838f4bae8341feadab
sonam-niit <sonam.gravity@gmail.com>
2024-01-11 15:56:37 +0530

    Maven and MongoDB

---
+++ Phase2/session4/src/main/java/com/cisco/session4/App.java
@@ -0,0 +1,14 @@
+package com.cisco.session4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World From Maven!" );
+        System.out.println("Be Exceptional");
+    }
+}
