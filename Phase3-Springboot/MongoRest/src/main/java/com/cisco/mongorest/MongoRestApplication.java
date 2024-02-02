8bcf8a164807b742a0e8884d91333591877db647
sonam-niit <sonam.gravity@gmail.com>
2024-02-02 19:58:00 +0530

    Swagger Implemented in all Restfull Applications

--- Phase3-Springboot/MongoRest/src/main/java/com/cisco/mongorest/MongoRestApplication.java
+++ Phase3-Springboot/MongoRest/src/main/java/com/cisco/mongorest/MongoRestApplication.java
@@ -3,11 +3,14 @@ package com.cisco.mongorest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
 @SpringBootApplication
 public class MongoRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoRestApplication.class, args);
 	}
-
 }
